372b557ddfbbef235480455772467d485b5b9e14
Alexander Konovalov <shuryak@protonmail.com>
2019-02-20 12:45:25 +0300

    ArrayList

---
+++ Collections/ArrayList/DynamicArrays/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DynamicArrays
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Dog(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Bark(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(Name + ": *barking*");
+            }
+        }
+    }
+}
---
+++ Collections/ArrayList/DynamicArrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections; // Attention!
+
+namespace DynamicArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Rocky = new Dog("Rocky", 2);
+
+            int[] simpleArray = new int[] { 8, 10, 30 };
+            ArrayList firstList = new ArrayList(); // Attention!
+
+            firstList.Add("Hello, World!");
+            firstList.Add(5);
+            firstList.Add(10.2);
+            firstList.Add(Rocky);
+            firstList.Add(simpleArray); // Attention!
+            firstList.AddRange(simpleArray); // Attention!
+
+            // Here we can no longer add simpleArray elements separately, but only can add the array simpleArray itself as an element of the collection:
+            ArrayList secondList = new ArrayList() { "Hello, World!", 5, 10.2, Rocky, simpleArray };
+
+            Console.WriteLine("Decomposing the firstList collection by type:\n");
+            foreach (object i in firstList)
+            {
+                Console.WriteLine(i.GetType());
+            }
+            Console.WriteLine("\nCount of elements in the collection: " + firstList.Count);
+
+            Console.WriteLine("\nDecomposing the secondList collection by type:\n");
+            foreach (object i in secondList)
+            {
+                Console.WriteLine(i.GetType());
+            }
+            Console.WriteLine("\nCount of elements in the collection: " + secondList.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
