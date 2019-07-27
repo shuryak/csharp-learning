67a3622eea85635e5f5e0fce607043cc512df360
Alexander Konovalov <shuryak@protonmail.com>
2019-07-27 11:04:11 +0300

    Edited AssemblyInfo for Collections/IEnumerable & IEnumerator

--- Collections/IEnumerable & IEnumerator/IEnumerable and IEnumerator/Properties/AssemblyInfo.cs
+++ Collections/IEnumerable & IEnumerator/IEnumerable and IEnumerator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IEnumerable and IEnumerator")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("shuryak © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
