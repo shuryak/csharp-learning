460c79a7866fc37a447f753f59b7d3d5990208c7
Alexander Konovalov <shuryak@protonmail.com>
2019-08-01 16:43:41 +0300

    Edited code in OOP/Generics

--- OOP/Generics/Generics/Program.cs
+++ OOP/Generics/Generics/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generics
 {
@@ -43,7 +39,7 @@ namespace Generics
         }
     }
 
-    public class UniversalArray<T>
+    public class UniversalArray<T> where T : struct // Only for structs (primitives are structures)
     {
         T[] array = new T[10];
         int index = 0;
