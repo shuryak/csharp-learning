653dcc962baf6570934fc580cdb6b6c43fd22186
Alexander Konovalov <shuryak@protonmail.com>
2019-08-09 13:08:15 +0300

    Exception filters

---
+++ Exceptions/Exception filters/ExcFilters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExcFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+
+            try
+            {
+                Console.WriteLine("Result: " + (x / y));
+            }
+            catch (DivideByZeroException) when (x == 0 && y == 0)
+            {
+                Console.WriteLine("0 / 0 is undefined.");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
